dfb8b899207a19a8e79c9a2f65a1c8f305941c8d
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2024-07-30 16:08:45 -0500

    Update certificates.php

--- reports/certificates.php
+++ reports/certificates.php
@@ -1,8 +1,84 @@
 <?php
 require_once('../../../config.php');
+require_once('../lib.php');  // Asegúrate de incluir el archivo con las funciones de exportación.
 require_login();
 $context = context_system::instance();
 require_capability('moodle/site:config', $context);
+
+$category = optional_param('category', '', PARAM_INT);
+$course = optional_param('course', '', PARAM_INT);
+$firstname = optional_param('firstname', '', PARAM_TEXT);
+$lastname = optional_param('lastname', '', PARAM_TEXT);
+$format = optional_param('format', 'excel', PARAM_TEXT); // Nuevo parámetro para el formato de descarga
+
+$params = [];
+$sql = "SELECT
+            CerGene.id AS uniqueid,
+            usua.idnumber AS cedula,
+            usua.firstname AS nombres,
+            usua.lastname AS apellidos,
+            usua.institution AS clinica,
+            usua.department AS area,
+            Curso.fullname AS nombrecurso,
+            FROM_UNIXTIME(CerGene.timecreated) AS fecha,
+            CatCurso.name AS categoriacurso
+        FROM
+            {customcert} AS CusCert
+        JOIN
+            {course} AS Curso ON Curso.id = CusCert.course
+        JOIN
+            {customcert_issues} CerGene ON CerGene.customcertid = CusCert.id
+        JOIN
+            {user} usua ON usua.id = CerGene.userid
+        JOIN
+            {course_categories} CatCurso ON Curso.category = CatCurso.id
+        WHERE
+            usua.idnumber <> ''";
+
+if ($category) {
+    $sql .= " AND CatCurso.id = :category";
+    $params['category'] = $category;
+}
+if ($course) {
+    $sql .= " AND Curso.id = :course";
+    $params['course'] = $course;
+}
+if ($firstname) {
+    $sql .= " AND usua.firstname LIKE :firstname";
+    $params['firstname'] = "$firstname%";
+}
+if ($lastname) {
+    $sql .= " AND usua.lastname LIKE :lastname";
+    $params['lastname'] = "$lastname%";
+}
+
+$records = $DB->get_records_sql($sql, $params);
+
+// Datos del reporte
+$data = new stdClass();
+$data->tabhead = ['Cedula', 'Nombres', 'Apellidos', 'Clinica', 'Area', 'NombreCurso', 'Fecha', 'CategoriaCurso'];
+$data->table = [];
+foreach ($records as $record) {
+    $data->table[] = [
+        $record->cedula,
+        $record->nombres,
+        $record->apellidos,
+        $record->clinica,
+        $record->area,
+        $record->nombrecurso,
+        $record->fecha,
+        $record->categoriacurso
+    ];
+}
+
+if (optional_param('download', '', PARAM_TEXT)) {
+    if ($format === 'csv') {
+        attendance_exporttocsv($data, 'certificates_report');
+    } else {
+        attendance_exporttotableed($data, 'certificates_report', $format);
+    }
+    exit;
+}
 
 $PAGE->set_url(new moodle_url('/blocks/reports_custom/reports/certificates.php'));
 $PAGE->set_context($context);
@@ -14,12 +90,6 @@ $PAGE->requires->css(new moodle_url('/blocks/reports_custom/reports/styles.css')
 
 $perpage = 100; 
 $page = optional_param('page', 0, PARAM_INT); 
-
-$category = optional_param('category', '', PARAM_INT);
-$course = optional_param('course', '', PARAM_INT);
-$firstname = optional_param('firstname', '', PARAM_TEXT);
-$lastname = optional_param('lastname', '', PARAM_TEXT);
-$format = optional_param('format', 'excel', PARAM_TEXT); // Nuevo parámetro para el formato de descarga
 
 echo $OUTPUT->header();
 
@@ -82,77 +152,8 @@ echo '</form>';
 
 echo '<div id="reportData">';
 
-$params = [];
-$sql = "SELECT
-            CerGene.id AS uniqueid,
-            usua.idnumber AS cedula,
-            usua.firstname AS nombres,
-            usua.lastname AS apellidos,
-            usua.institution AS clinica,
-            usua.department AS area,
-            Curso.fullname AS nombrecurso,
-            FROM_UNIXTIME(CerGene.timecreated) AS fecha,
-            CatCurso.name AS categoriacurso
-        FROM
-            {customcert} AS CusCert
-        JOIN
-            {course} AS Curso ON Curso.id = CusCert.course
-        JOIN
-            {customcert_issues} CerGene ON CerGene.customcertid = CusCert.id
-        JOIN
-            {user} usua ON usua.id = CerGene.userid
-        JOIN
-            {course_categories} CatCurso ON Curso.category = CatCurso.id
-        WHERE
-            usua.idnumber <> ''";
-
-if ($category) {
-    $sql .= " AND CatCurso.id = :category";
-    $params['category'] = $category;
-}
-if ($course) {
-    $sql .= " AND Curso.id = :course";
-    $params['course'] = $course;
-}
-if ($firstname) {
-    $sql .= " AND usua.firstname LIKE :firstname";
-    $params['firstname'] = "$firstname%";
-}
-if ($lastname) {
-    $sql .= " AND usua.lastname LIKE :lastname";
-    $params['lastname'] = "$lastname%";
-}
-
-$totalcount = $DB->count_records_sql("SELECT COUNT(*) FROM ($sql) AS total", $params);
-$records = $DB->get_records_sql($sql, $params, $page * $perpage, $perpage);
-
-// Datos del reporte
-$data = new stdClass();
-$data->course = ''; // Establecer curso actual
-$data->group = ''; // Establecer grupo actual
-$data->tabhead = ['Cedula', 'Nombres', 'Apellidos', 'Clinica', 'Area', 'NombreCurso', 'Fecha', 'CategoriaCurso'];
-$data->table = [];
-foreach ($records as $record) {
-    $data->table[] = [
-        $record->cedula,
-        $record->nombres,
-        $record->apellidos,
-        $record->clinica,
-        $record->area,
-        $record->nombrecurso,
-        $record->fecha,
-        $record->categoriacurso
-    ];
-}
-
-if (optional_param('download', '', PARAM_TEXT)) {
-    if ($format === 'csv') {
-        attendance_exporttocsv($data, 'certificates_report');
-    } else {
-        attendance_exporttotableed($data, 'certificates_report', $format);
-    }
-    exit;
-}
+$totalcount = count($records);
+$records = array_slice($records, $page * $perpage, $perpage);
 
 $table = new html_table();
 $table->head = [
