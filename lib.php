cc80255ebd515dcda2983544d9ee2424e9846c44
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2024-07-30 16:57:38 -0500

    Update lib.php

--- lib.php
+++ lib.php
@@ -1,27 +1,28 @@
 <?php
+
 /**
  * Generate worksheet for report export
  *
  * @param stdClass $data The data for the report
  * @param string $filename The name of the file
  * @param string $format excel|ods
- *
  */
-function attendance_exporttotableed($data, $filename, $format)
-{
+function attendance_exporttotableed($data, $filename, $format) {
     global $CFG;
 
     if ($format === 'excel') {
-        require_once("$CFG->libdir/excellib.class.php");
+        require_once($CFG->libdir.'/excellib.class.php');
         $filename .= ".xls";
         $workbook = new MoodleExcelWorkbook("-");
     } else {
-        require_once("$CFG->libdir/odslib.class.php");
+        require_once($CFG->libdir.'/odslib.class.php');
         $filename .= ".ods";
         $workbook = new MoodleODSWorkbook("-");
     }
+
     // Sending HTTP headers.
     $workbook->send($filename);
+
     // Creating the first worksheet.
     $myxls = $workbook->add_worksheet('Report');
     // Format types.
@@ -29,32 +30,26 @@ function attendance_exporttotableed($data, $filename, $format)
     $formatbc->set_bold(1);
 
     // Writing headers.
-    $i = 0;
     foreach ($data->tabhead as $j => $header) {
-        $myxls->write($i, $j, $header, $formatbc);
+        $myxls->write(0, $j, $header, $formatbc);
     }
 
     // Writing data.
-    $i = 1;
-    foreach ($data->table as $row) {
+    foreach ($data->table as $i => $row) {
         foreach ($row as $j => $cell) {
-            $myxls->write($i, $j, $cell);
+            $myxls->write($i + 1, $j, $cell);
         }
-        $i++;
     }
     $workbook->close();
 }
-
 
 /**
  * Generate csv for report export
  *
  * @param stdClass $data The data for the report
  * @param string $filename The name of the file
- *
  */
-function attendance_exporttocsv($data, $filename)
-{
+function attendance_exporttocsv($data, $filename) {
     $filename .= ".csv";
 
     header("Content-Type: text/csv; charset=utf-8");
@@ -64,6 +59,11 @@ function attendance_exporttocsv($data, $filename)
     header("Pragma: public");
 
     $output = fopen('php://output', 'w');
+    if ($output === false) {
+        debugging("Failed to open output stream", DEBUG_DEVELOPER);
+        return;
+    }
+
     // Output headers.
     fputcsv($output, $data->tabhead);
 
