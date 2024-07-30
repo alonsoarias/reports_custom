f5d7fd8d5684a74f75886ac73d1f1ae75f4a7f02
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2024-07-30 17:57:40 -0500

    Update lib.php

--- lib.php
+++ lib.php
@@ -5,16 +5,16 @@ require_once("$CFG->libdir/odslib.class.php");
 require_once("$CFG->libdir/csvlib.class.php");
 
 /**
- * Export report to .xlsx or .ods format.
+ * Export data to .xlsx or .ods format.
  *
- * @param stdClass $data The data for the report.
+ * @param array $headers The headers for the data.
+ * @param array $rows The rows of data.
  * @param string $filename The base name of the file without extension.
  * @param string $format The format of the export: 'excel' or 'ods'.
+ * @param string $sheetname The name of the worksheet.
  */
-function attendance_exporttotableed($data, $filename, $format) {
+function export_to_spreadsheet($headers, $rows, $filename, $format, $sheetname = 'Sheet1') {
     global $CFG;
-
-    debugging("Starting attendance_exporttotableed function", DEBUG_DEVELOPER);
 
     // Clean the output buffer to fix the headers already sent error
     while (ob_get_level()) {
@@ -22,38 +22,31 @@ function attendance_exporttotableed($data, $filename, $format) {
     }
 
     if ($format === 'excel') {
-        debugging("Loading Excel library", DEBUG_DEVELOPER);
-        require_once($CFG->libdir.'/excellib.class.php');
         $filename .= ".xlsx";
         $workbook = new MoodleExcelWorkbook("-");
     } else {
-        debugging("Loading ODS library", DEBUG_DEVELOPER);
-        require_once($CFG->libdir.'/odslib.class.php');
         $filename .= ".ods";
         $workbook = new MoodleODSWorkbook("-");
     }
 
-    debugging("Sending workbook headers for $filename", DEBUG_DEVELOPER);
+    // Sending HTTP headers.
     $workbook->send($filename);
 
-    $sheettitle = get_string('report', 'block_reports_custom');
-    $worksheet = $workbook->add_worksheet($sheettitle);
+    // Creating the worksheet.
+    $worksheet = $workbook->add_worksheet($sheetname);
 
-    // Define the format for the header
+    // Define the format for the header.
     $formatbc = $workbook->add_format(array('bold' => 1));
-    $headers = $data->tabhead;
 
-    // Write the headers
-    debugging("Writing headers", DEBUG_DEVELOPER);
+    // Write the headers.
     $col = 0;
     foreach ($headers as $header) {
         $worksheet->write(0, $col++, $header, $formatbc);
     }
 
-    // Write the data
-    debugging("Writing data", DEBUG_DEVELOPER);
+    // Write the data.
     $row = 1;
-    foreach ($data->table as $record) {
+    foreach ($rows as $record) {
         $col = 0;
         foreach ($record as $value) {
             $worksheet->write($row, $col++, $value);
@@ -61,26 +54,24 @@ function attendance_exporttotableed($data, $filename, $format) {
         $row++;
     }
 
-    debugging("Closing workbook", DEBUG_DEVELOPER);
     $workbook->close();
-    debugging("Workbook closed successfully", DEBUG_DEVELOPER);
 }
 
 /**
- * Export report to .txt format.
+ * Export data to .csv format.
  *
- * @param stdClass $data The data for the report.
+ * @param array $headers The headers for the data.
+ * @param array $rows The rows of data.
  * @param string $filename The base name of the file.
  */
-function attendance_exporttocsv($data, $filename) {
-    debugging("Starting attendance_exporttocsv function", DEBUG_DEVELOPER);
-
-    $filename .= ".csv";
-
-    // Prevent the error "headers already sent"
+function export_to_csv($headers, $rows, $filename) {
+    // Clean the output buffer to fix the headers already sent error
     while (ob_get_level()) {
         ob_end_clean();
     }
+
+    $filename .= ".csv";
+
     header("Content-Type: text/csv; charset=utf-8");
     header("Content-Disposition: attachment; filename=\"$filename\"");
     header("Expires: 0");
@@ -93,18 +84,14 @@ function attendance_exporttocsv($data, $filename) {
         return;
     }
 
-    // Output headers
-    debugging("Writing CSV headers", DEBUG_DEVELOPER);
-    fputcsv($output, $data->tabhead);
+    // Output headers.
+    fputcsv($output, $headers);
 
-    // Output data
-    debugging("Writing CSV data", DEBUG_DEVELOPER);
-    foreach ($data->table as $record) {
+    // Output data.
+    foreach ($rows as $record) {
         fputcsv($output, $record);
     }
 
     fclose($output);
-    debugging("CSV file generated successfully", DEBUG_DEVELOPER);
 }
-
 ?>
