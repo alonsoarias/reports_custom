b803a0e1bc2b036b929fd5ae7b1c3ab385654bfb
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2024-08-07 12:43:45 -0500

    Update access.php

--- db/access.php
+++ db/access.php
@@ -1,20 +1,32 @@
 <?php
-$capabilities = array(
-    'block/reports_custom:addinstance' => array(
+
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = [
+    'block/reports_custom:addinstance' => [
+        'riskbitmask' => RISK_SPAM | RISK_XSS,
         'captype' => 'write',
-        'contextlevel' => CONTEXT_BLOCK,
-        'archetypes' => array(
-            'editingteacher' => CAP_ALLOW,
-            'manager' => CAP_ALLOW
-        ),
-        'clonepermissionsfrom' => 'moodle/site:manageblocks'
-    ),
-    'block/reports_custom:myaddinstance' => array(
+        'contextlevel' => CONTEXT_BLOCK,  // Correctamente configurado para el contexto de bloque
+        'archetypes' => [
+            'manager' => CAP_ALLOW,  // Solo los gestores pueden añadir instancias del bloque
+        ],
+        'clonepermissionsfrom' => 'moodle/site:manageblocks',  // Clonar permisos de manageblocks
+    ],
+
+    'block/reports_custom:myaddinstance' => [
+        'riskbitmask' => RISK_SPAM | RISK_XSS,
         'captype' => 'write',
-        'contextlevel' => CONTEXT_BLOCK,
-        'archetypes' => array(
-            'user' => CAP_ALLOW,
-        ),
-        'clonepermissionsfrom' => 'moodle/my:manageblocks'
-    ),
-);
+        'contextlevel' => CONTEXT_BLOCK,  // Aplicable también a CONTEXT_BLOCK
+        'archetypes' => [
+            'manager' => CAP_ALLOW,  // Solo los gestores pueden añadir instancias del bloque en su página 'Mi Moodle'
+        ],
+    ],
+
+    'block/reports_custom:viewreports' => [
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_SYSTEM,  // Usando el contexto del sistema para la visualización de reportes
+        'archetypes' => [
+            'manager' => CAP_ALLOW,  // Solo los gestores tienen permiso para ver los reportes
+        ],
+    ],
+];
