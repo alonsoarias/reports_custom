b7b2bb084c61df59337580d7d8a0697aed3e6cb5
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2024-08-07 12:16:37 -0500

    Create block_reports_custom.php

---
+++ lang/es/block_reports_custom.php
@@ -0,0 +1,54 @@
+<?php
+
+// Nombre del Plugin
+$string['pluginname'] = 'Bloque de Informes Personalizados';
+$string['reports_custom'] = 'Informes Personalizados';
+
+// Títulos de Informes
+$string['certificates_report'] = 'Certificados Generados por Cursos';
+$string['progress_report'] = 'Cursos - Avance por usuario detallado - Calificación y fecha';
+
+// Capacidades
+$string['reports_custom:addinstance'] = 'Agregar un nuevo bloque de Informes Personalizados';
+$string['reports_custom:myaddinstance'] = 'Agregar un nuevo bloque de Informes Personalizados a la página Mi Moodle';
+
+// Encabezados de Tablas
+$string['header_cedula'] = 'Cédula';
+$string['header_nombres'] = 'Nombres';
+$string['header_apellidos'] = 'Apellidos';
+$string['header_clinica'] = 'Clínica';
+$string['header_area'] = 'Área';
+$string['header_nombrecurso'] = 'Nombre del Curso';
+$string['header_fecha'] = 'Fecha';
+$string['header_categoriacurso'] = 'Categoría del Curso';
+$string['header_user_type'] = 'Tipo de Usuario';
+$string['header_usuario'] = 'Usuario';
+$string['header_nombre'] = 'Nombre';
+$string['header_apellido'] = 'Apellido';
+$string['header_nombre_completo'] = 'Nombre Completo';
+$string['header_categoria'] = 'Categoría';
+$string['header_curso'] = 'Curso';
+$string['header_item'] = 'Ítem';
+$string['header_calificacion'] = 'Calificación';
+
+// Opciones de Formulario
+$string['option_todos'] = 'Todos';
+$string['option_category'] = 'Categoría';
+$string['option_course'] = 'Curso';
+$string['option_usertype'] = 'Tipo de Usuario';
+$string['option_nombre'] = 'Nombre';
+$string['option_apellido'] = 'Apellido(s)';
+$string['option_download_format'] = 'Formato de descarga';
+$string['option_download_excel'] = 'Excel';
+$string['option_download_ods'] = 'ODS';
+$string['option_download_csv'] = 'CSV';
+
+// Otros
+$string['report_title'] = 'Informe';
+$string['report_heading'] = 'Informe';
+$string['btn_descargar'] = 'Descargar';
+$string['alphabet_all'] = 'Todos';
+$string['total_records'] = 'Total de Registros';
+
+
+?>
