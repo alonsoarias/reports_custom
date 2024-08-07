4ad300a35c3e867f5c86296726e3ef59ed33d364
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2024-08-07 12:48:52 -0500

    Update access.php

--- db/access.php
+++ db/access.php
@@ -6,27 +6,43 @@ $capabilities = [
     'block/reports_custom:addinstance' => [
         'riskbitmask' => RISK_SPAM | RISK_XSS,
         'captype' => 'write',
-        'contextlevel' => CONTEXT_BLOCK,  // Correctamente configurado para el contexto de bloque
+        'contextlevel' => CONTEXT_BLOCK,
         'archetypes' => [
-            'manager' => CAP_ALLOW,  // Solo los gestores pueden añadir instancias del bloque
+            'manager' => CAP_ALLOW
         ],
-        'clonepermissionsfrom' => 'moodle/site:manageblocks',  // Clonar permisos de manageblocks
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
     ],
 
     'block/reports_custom:myaddinstance' => [
         'riskbitmask' => RISK_SPAM | RISK_XSS,
         'captype' => 'write',
-        'contextlevel' => CONTEXT_BLOCK,  // Aplicable también a CONTEXT_BLOCK
+        'contextlevel' => CONTEXT_BLOCK,
         'archetypes' => [
-            'manager' => CAP_ALLOW,  // Solo los gestores pueden añadir instancias del bloque en su página 'Mi Moodle'
-        ],
+            'manager' => CAP_ALLOW
+        ]
     ],
 
-    'block/reports_custom:viewreports' => [
+    'block/reports_custom:view' => [
         'captype' => 'read',
-        'contextlevel' => CONTEXT_SYSTEM,  // Usando el contexto del sistema para la visualización de reportes
+        'contextlevel' => CONTEXT_BLOCK,
         'archetypes' => [
-            'manager' => CAP_ALLOW,  // Solo los gestores tienen permiso para ver los reportes
-        ],
+            'manager' => CAP_ALLOW
+        ]
     ],
+
+    'moodle/block:edit' => [
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => [
+            'manager' => CAP_ALLOW
+        ]
+    ],
+
+    'moodle/block:view' => [
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => [
+            'manager' => CAP_ALLOW
+        ]
+    ]
 ];
