9f91e3eeb8add3623521f248887fe3fa6a383205
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2024-08-07 09:55:09 -0500

    Update get_courses.php

--- reports/get_courses.php
+++ reports/get_courses.php
@@ -2,8 +2,15 @@
 require_once('../../../config.php');
 require_login();
 
-$categoryid = required_param('category', PARAM_INT);
-$courses = $DB->get_records('course', array('category' => $categoryid), 'fullname ASC', 'id, fullname');
+$categoryid = optional_param('category', 0, PARAM_INT); // Cambiado a optional_param con valor por defecto 0
+
+if ($categoryid) {
+    // Si se seleccionó una categoría, obtener cursos de esa categoría
+    $courses = $DB->get_records('course', array('category' => $categoryid), 'fullname ASC', 'id, fullname');
+} else {
+    // Si no se seleccionó categoría, obtener todos los cursos
+    $courses = $DB->get_records('course', null, 'fullname ASC', 'id, fullname');
+}
 
 echo json_encode(array_values($courses));
 ?>
