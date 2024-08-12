f701b0a237e6fe27dbc2bbe8c56560369082fcd6
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2024-08-12 17:19:08 -0500

    Update version.php

--- version.php
+++ version.php
@@ -1,5 +1,5 @@
 <?php
-$plugin->version = 2023073007;
+$plugin->version = 2024081200;
 $plugin->requires = 2021051700; // Requires this Moodle version - 3.11.
 $plugin->component = 'block_reports_custom';
 $plugin->maturity = MATURITY_STABLE;
