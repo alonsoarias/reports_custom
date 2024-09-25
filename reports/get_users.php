5f8f240433bc8ed464a385b32ce2ba206bee184b
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2024-09-25 00:50:09 -0500

    Update get_users.php

--- reports/get_users.php
+++ reports/get_users.php
@@ -1,33 +1,50 @@
 <?php
 require_once('../../../config.php');
+require_once('../lib.php');
 require_login();
 
 $courseid = optional_param('course', 0, PARAM_INT);
 $categoryid = optional_param('category', 0, PARAM_INT);
 $usertype = optional_param('usertype', '', PARAM_TEXT);
 
+// Obtener las categorías permitidas para el usuario actual
+$allowedCategories = get_allowed_categories_for_user($USER->id);
+
 $params = [];
 $sql = "SELECT DISTINCT u.id, CONCAT(u.firstname, ' ', u.lastname) AS fullname
         FROM {user} u
         JOIN {user_enrolments} ue ON ue.userid = u.id
-        JOIN {enrol} e ON e.id = ue.enrolid";
+        JOIN {enrol} e ON e.id = ue.enrolid
+        JOIN {course} c ON c.id = e.courseid";
+
+$where = ["u.deleted = 0"];
 
 if ($courseid) {
-    $sql .= " WHERE e.courseid = :courseid";
+    $where[] = "e.courseid = :courseid";
     $params['courseid'] = $courseid;
 } elseif ($categoryid) {
-    $sql .= " JOIN {course} c ON c.id = e.courseid WHERE c.category = :categoryid";
+    $where[] = "c.category = :categoryid";
     $params['categoryid'] = $categoryid;
 }
 
 if ($usertype) {
-    $sql .= " JOIN {user_info_data} d1 ON d1.userid = u.id
-              JOIN {user_info_field} f1 ON d1.fieldid = f1.id AND f1.shortname = 'user_type'
-              WHERE d1.data = :usertype";
+    $sql .= " LEFT JOIN {user_info_data} d1 ON d1.userid = u.id
+              LEFT JOIN {user_info_field} f1 ON d1.fieldid = f1.id AND f1.shortname = 'user_type'";
+    $where[] = "(d1.data = :usertype OR (d1.data IS NULL AND :usertype = 'No asignado'))";
     $params['usertype'] = $usertype;
 }
+
+// Aplicar restricciones de categorías permitidas
+if ($allowedCategories !== null) {
+    $where[] = "c.category IN (" . implode(',', $allowedCategories) . ")";
+}
+
+if (!empty($where)) {
+    $sql .= " WHERE " . implode(" AND ", $where);
+}
+
+$sql .= " ORDER BY fullname ASC";
 
 $users = $DB->get_records_sql($sql, $params);
 
 echo json_encode(array_values($users));
-?>
